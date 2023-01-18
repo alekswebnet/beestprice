9e7c3adc7647703da12cc072089bb7950a0692c0
alekswebnet <alekswebnet@gmail.com>
2023-01-18 03:52:24 +0200

    Try to fix puppeteer plugins errors

--- server/services/products.service.ts
+++ server/services/products.service.ts
@@ -6,43 +6,43 @@ import currency from 'currency.js';
 import puppeteerVanilla from 'puppeteer';
 import { addExtra } from 'puppeteer-extra';
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/chrome.app');
+import('puppeteer-extra-plugin-stealth/evasions/chrome.app/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/chrome.csi');
+import('puppeteer-extra-plugin-stealth/evasions/chrome.csi/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes');
+import('puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/chrome.runtime');
+import('puppeteer-extra-plugin-stealth/evasions/chrome.runtime/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow');
+import('puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/media.codecs');
+import('puppeteer-extra-plugin-stealth/evasions/media.codecs/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency');
+import('puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/navigator.languages');
+import('puppeteer-extra-plugin-stealth/evasions/navigator.languages/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/navigator.permissions');
+import('puppeteer-extra-plugin-stealth/evasions/navigator.permissions/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/navigator.plugins');
+import('puppeteer-extra-plugin-stealth/evasions/navigator.plugins/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/navigator.vendor');
+import('puppeteer-extra-plugin-stealth/evasions/navigator.vendor/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver');
+import('puppeteer-extra-plugin-stealth/evasions/navigator.webdriver/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/sourceurl');
+import('puppeteer-extra-plugin-stealth/evasions/sourceurl/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/user-agent-override');
+import('puppeteer-extra-plugin-stealth/evasions/user-agent-override/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/webgl.vendor');
+import('puppeteer-extra-plugin-stealth/evasions/webgl.vendor/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions');
+import('puppeteer-extra-plugin-stealth/evasions/window.outerdimensions/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-stealth/evasions/defaultArgs');
+import('puppeteer-extra-plugin-stealth/evasions/defaultArgs/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-user-preferences');
+import('puppeteer-extra-plugin-user-preferences/index');
 // @ts-ignore
-import('puppeteer-extra-plugin-user-data-dir');
+import('puppeteer-extra-plugin-user-data-dir/index');
 
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import AdblockerPlugin  from 'puppeteer-extra-plugin-adblocker'
